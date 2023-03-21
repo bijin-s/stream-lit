d44fcc669b277b9c649e3f3b32b7927ecd3d4169
bijin-s <127382944+bijin-s@users.noreply.github.com>
2023-03-22 01:57:08 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,25 @@
 import streamlit as st
 import os.path
 import pathlib
+
+from google.oauth2 import service_account
+from google.cloud import storage
+
+# Create API client.
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"]
+)
+client = storage.Client(credentials=credentials)
+
+# Retrieve file contents.
+# Uses st.cache_data to only rerun when the query changes or after 10 min.
+@st.cache_data(ttl=600)
+def read_file(bucket_name, file_path):
+    bucket = client.bucket(bucket_name)
+    content = bucket.blob(file_path).download_as_string().decode("utf-8")
+    return content
+
+bucket_name = "streamlit-bucket"
 st.write("""
 # File Picker
 """)
@@ -14,6 +33,22 @@ if uploaded_file is not None:
         st.session_state["preview"] += data[i]
 preview = st.text_area("CSV Preview", "", height=150, key="preview")
 upload_state = st.text_area("Upload State", "", key="upload_state")
+def upload_to_bucket(blob_name, path_to_file, bucket_name):
+    """ Upload data to a bucket"""
+     
+    # Explicitly use service account credentials by specifying the private key
+    # file.
+    storage_client = storage.Client.from_service_account_json(
+        'creds.json')
+
+    #print(buckets = list(storage_client.list_buckets())
+    bucket = storage.Bucket(client, 'example-bucket-skytowner')
+
+    blob = bucket.blob('uploaded_sample.csv')
+    blob.upload_from_filename('sample.csv')
+    
+    #returns a public url
+    return blob.public_url
 def upload():
     if uploaded_file is None:
         st.session_state["upload_state"] = "Upload a file first!"
@@ -22,8 +57,11 @@ def upload():
         parent_path = pathlib.Path(__file__).parent.parent.resolve()           
         save_path = os.path.join(parent_path, "data")
         complete_name = os.path.join(save_path, uploaded_file.name)
+        upload_to_bucket(
         destination_file = open(complete_name, "w")
         destination_file.write(data)
         destination_file.close()
         st.session_state["upload_state"] = "Saved " + complete_name + " successfully!"
+     
 st.button("Upload file to Sandbox", on_click=upload)
+
